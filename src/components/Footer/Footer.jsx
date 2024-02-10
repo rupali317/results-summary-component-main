31eedc8fc0472fc49631739474832d5d5d6b85f2
PriyankaRC16 <rupali317@gmail.com>
2024-02-10 19:36:32 +0800

    Setting up the files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+/*<div class="attribution">
+Challenge by
+<a
+  href="https://www.frontendmentor.io?ref=challenge"
+  target="_blank"
+  rel="noreferrer"
+  >Frontend Mentor</a
+>. Coded by
+<a href="https://www.linkedin.com/in/rupali-rc/">Rupali Roy Choudhury</a>.
+</div>*/
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const main = ReactDOM.createRoot(document.getElementById("main"));
+main.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+const footer = ReactDOM.createRoot(document.getElementById("footer"));
+footer.render(<div>Dummy</div>);
 
 reportWebVitals(console.log);
