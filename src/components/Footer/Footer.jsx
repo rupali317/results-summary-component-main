71aebc8218d870caf5a4e3c5a62a86e95fe1b853
PriyankaRC16 <rupali317@gmail.com>
2024-02-12 10:04:09 +0800

    Improving the footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,15 @@
 export const Footer = () => {
   return (
     <small>
-      Challenge by
+      Challenge by{" "}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
         target="_blank"
         rel="noreferrer"
       >
-        {" "}
-        Frontend Mentor
-      </a>
-      . Coded by{" "}
+        Frontend Mentor.
+      </a>{" "}
+      Coded by{" "}
       <a href="https://www.linkedin.com/in/rupali-rc/">Rupali Roy Choudhury</a>
     </small>
   );
